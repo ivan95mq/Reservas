38f83db2c77fc86d98d2fec42d4b3c36e3985a30
Iván Moreno Quiros <43568081+ivan95mq@users.noreply.github.com>
2022-09-24 19:39:10 +0200

    Inclusion de headers

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-
-const reservas = require("src/parse/parse.js");
 // myJQuery
-$ = function(selector) {
+$ = function (selector) {
     var elem = document.querySelector(selector);
     elem.on = elem.addEventListener;
     elem.off = elem.removeEventListener;
-    
+
     return elem;
-  }
-$.elem()
+}
+
+const fs = require('fs');
+const parser = require('csv-parser');
+const data = [];
+
+fs.createReadStream('./assets/reservas.csv')
+    .pipe(parser({
+        separator: ';',
+        newline: '\n',
+        skipLines: 3,
+        headers: ["Booking","Booking date","Acronym","Name","Address","Postal code","City","Country","VAT number","Email","Telephone","Reference","Adults","Children","Infants","Origin","Arrival","Departure","Nights","Channel commission","Total","Extras","Supplements","Early booking discount","Long stay discount","Last minute discount","Standard discount","Promotion discount","Booking fee","Cleaning fee","Check-In fee","Short-stay fee","Tax","Paid","Security Deposit","Owner comission","Stay","First payment","Property","Owner","Owner's share","Amount for the agency","Rate","Language"],
+    }))
+    .on('data', row => data.push(row))
+    .on('end', () => {
+        
+        data.forEach(element => {
+
+            console.log(element.name);
+            element.forEach(e => {
+                
+            });
+        });
+    } )
